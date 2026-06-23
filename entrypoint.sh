0bf0a6399789ebb444a6f712179e20106b59ffd6
mickkc <contact@mickkc.dev>
2026-06-23 15:46:35 +0200

    feat: automatically create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@
 python manage.py makemigrations
 python manage.py migrate
 
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
+
 gunicorn --bind 0.0.0.0:${BACKEND_PORT} conduit.wsgi:application
