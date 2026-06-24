53725d371337c0795347c1884987fcaf509e4e6c
mickkc <contact@mickkc.dev>
2026-06-24 15:05:57 +0200

    feat: added collectstatic to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate
+python manage.py collectstatic --noinput
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
     python manage.py createsuperuser --no-input || true
